abea5b0d06b16672d9fa93dd4e1430842cf7aa30
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-29 20:55:51 +0530

    Create DAO.java

---
+++ com/app/dao/DAO.java
@@ -0,0 +1,9 @@
+package com.app.dao;
+
+public interface DAO {
+	public void saveDataUsingHallTicket();
+	public void saveDataUsingStudent();
+	public void loadDataUsingHallTicket();
+	public void loadDataUsingStudent();
+
+}
